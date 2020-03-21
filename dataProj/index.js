bd29829dc552cea602d7a31b8667d53ce25752ce
Brien Bledsoe <bab619@nyu.edu>
2020-03-21 19:30:17 -0400

    server has successfully received longitude and lat data

--- dataProj/index.js
+++ dataProj/index.js
@@ -28,8 +28,38 @@ the way we give the server access to our index.html file is by using Express to 
 /*app.use("/", (req,res) =>
   res.sendFile("/public/index.html", {root: __dirname})
 );
-*/ 
+*/
 
 app.use(express.static('public')); /*what we need to give here is a folder name, and were going to call it public,
 because we're going to remind ourselves that we put within the public directory is public accessible from the URL
 in this case itse localhost:3000 */
+
+app.use(express.json({limit: '1mb'})); /*
+code that allows the server to parse javaScript code
+
+we can also put some options in there to control or limit what is possible in terms of receiving
+data
+
+we can start by limitting the code server to only processing 1 megabyte of code
+*/
+app.post('/sending_data', (request, response) => {
+  //res.send('POST request to the homepage')
+  console.log(request.body);
+});
+/*When specifying a post, we are going to want to specify the address, where we want to receive that POST, as
+well as a callback function, where I'm going to look at the information coming in and send a response back
+
+now we are setting up the address, the endpoint for this particular route, where we want to receive
+post. We can call it anything.
+
+after the address, we need to set up a callback, (requests, response). We will be using the javaScript arrow syntax to create the
+function which makes everything cleaner and replaces the generic function key word in the express version.
+
+* so the function is here, and the request variable holds everything thats within that request. All the data thats being sent
+  any information we need to know about that particular client is sending the information
+
+* the response is a variable that we can use to send things back to the client.
+
+* now we must move fetch our post over to the /sending_data endpoint.
+    - so the client code, index.js and client index.html,
+*/
