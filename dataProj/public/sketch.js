26a9304a5cbbf96fdb08f24b6949bc25af72c50d
Brien Bledsoe <bab619@nyu.edu>
2020-05-10 01:39:09 -0400

    only 3 more videos

--- dataProj/public/sketch.js
+++ dataProj/public/sketch.js
@@ -6,7 +6,7 @@ if('geolocation' in navigator){
   console.log('geolocation available');
   navigator.geolocation.getCurrentPosition(async position => {
     lat = position.coords.latitude;
-    lon = position.coords.longitude
+    lon = position.coords.longitude;
     document.getElementById('latitude').textContent = lat.toFixed(2);
     document.getElementById('longitude').textContent = lon.toFixed(2);
     // const apiKey = 'bd3aebe7893aee9885545c96d06c993e'
@@ -15,11 +15,33 @@ if('geolocation' in navigator){
     const response = await fetch(api_url);
     const json = await response.json();
     console.log("This is json: ", json)
+    const air = json.air_quality.results[0] ? json.air_quality.results[0].measurements[0] : "Not available";
+    // const air = json.air_quality.results[0].measurements[0];
+    // console.log("Testing: ",air);
+
+    // const air = json.air_quality.results[0] ? json.air_quality.results[0].measurements[0] :
+    hideInfo()
+    function hideInfo() {
+      if(air === "Not available"){
+        console.log("inside first if")
+        var x = document.getElementById("airQuality");
+          x.style.display="none";
+      }
+
+    }
+    console.log("Testing: ",air);
+
     document.getElementById('summary').textContent = json.weather.current.weather[0].description;
     document.getElementById('temperature').textContent = json.weather.current.temp;
+    document.getElementById('aq_parameter').textContent = air.parameter ? air.parameter : "Not available";
+
+    // document.getElementById('aq_parameter').textContent = air.parameter;
+    document.getElementById('aq_value').textContent = air.value ? air.value: "Not available";
+    document.getElementById('aq_units').textContent = air.unit ? air.value: "Not available";
+    document.getElementById('aq_date').textContent = air.lastUpdated ? air.lastUpdated : "Not available";
     console.log("is anything happening: ", json);
 
-    // console.log("This is your position: ",position)
+    console.log("This is your position: ",position)
   });
 
   }else{
